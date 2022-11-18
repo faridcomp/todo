a6f61c9973ef022199a0decf8f7179e38062cd32
Farid <farid.ibrahim29@gmail.com>
2022-11-18 06:15:53 -0500

    2Version

--- myproject/todo/views.py
+++ myproject/todo/views.py
@@ -1,30 +1,26 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 
 from . models import Todo
 from . forms import TodoForm
 
+@login_required(login_url= ('accounts:accounts_login'))
 def index(request):
     todo_list = Todo.objects.order_by('id')
-    
     form = TodoForm()
-    
     context = {
         'todo_list': todo_list, 'form': form,
     }
-    
-    
     return render(request, 'todo/index.html', context)
 
 
 @require_POST
 def addTodo(request):
     form = TodoForm(request.POST)
-    
     if form.is_valid():
         new_todo = Todo(text=request.POST['text'])
         new_todo.save()
-    
     print(request.POST['text'])
     return redirect('index')
 
@@ -32,16 +28,12 @@ def completeTodo(request, todo_id):
     todo = Todo.objects.get(pk=todo_id)
     todo.complete = True
     todo.save()
-    
-    
     return redirect ('index')
 
 def deleteCompleted(request):
     Todo.objects.filter(complete__exact = True).delete()
-    
     return redirect('index')
 
 def deleteAll(request):
     Todo.objects.all().delete()
-    
     return redirect('index')
